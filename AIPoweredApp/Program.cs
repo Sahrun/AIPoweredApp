96354b1fedb4085d7e030c313929e5bcf63c83b9
Sahrun Nawawi <sahrun.nawawi@it.component.astra.co.id>
2025-02-06 14:23:34 +0700

    Add project files.

---
+++ AIPoweredApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+
+var pathConfig = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+var config = new ConfigurationBuilder()
+            .AddJsonFile(pathConfig, optional: false, reloadOnChange: true)
+            .Build();
+
+var builder = Kernel.CreateBuilder();
+
+builder.AddOpenAIChatCompletion("gpt-4o-mini", config["gpt.secreet"] ?? "");
+
+Kernel kernel = builder.Build();
+
+var chatService = kernel.GetRequiredService<IChatCompletionService>();
+
+ChatHistory chatHistory = new ChatHistory();
+
+
+while (true)
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.Write("User>> ");
+
+    var userMessage = Console.ReadLine();
+
+    chatHistory.AddUserMessage(userMessage);
+
+    var response = chatService.GetStreamingChatMessageContentsAsync(chatHistory, kernel: kernel);
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write("AI>> ");
+
+    string fullMessage = "";
+
+    await foreach (var chat in response)
+    {
+        Console.Write(chat);
+        fullMessage += chat;
+    }
+
+    Console.WriteLine();
+
+    chatHistory.AddAssistantMessage(fullMessage);
+}
---
+++ DeepSeekWithOllama/Program.cs
@@ -0,0 +1,17 @@
+﻿using Codeblaze.SemanticKernel.Connectors.Ollama;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel;
+
+var builder = Kernel.CreateBuilder().AddOllamaChatCompletion("deepseek-r1:1.5b", "http://localhost:11434");
+
+builder.Services.AddScoped<HttpClient>();
+var kernel = builder.Build();
+
+while (true)
+{
+    string input = "";
+    Console.WriteLine("Ask anything to Deepseek");
+    input = Console.ReadLine();
+    var response = await kernel.InvokePromptAsync(input);
+    Console.WriteLine($"\nDeepseek: {response}\n");
+}
